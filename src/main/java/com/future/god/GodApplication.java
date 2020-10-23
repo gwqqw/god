9086aed1c2ce4da6d714524d877225de8ce932d5
Andy Gong <andy.gong@signify.com>
2020-10-23 11:40:28 +0800

    Initial commit

---
+++ src/main/java/com/future/god/GodApplication.java
@@ -0,0 +1,13 @@
+package com.future.god;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodApplication.class, args);
+    }
+
+}
